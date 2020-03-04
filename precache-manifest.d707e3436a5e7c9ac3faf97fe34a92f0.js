40b22b9e99fe013de0a684471196d0cde5aa7faf
Victoria Aseff <vickase.a@gmail.com>
2020-03-04 00:26:44 -0300

    Updates

---
+++ precache-manifest.d707e3436a5e7c9ac3faf97fe34a92f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1887367b8e6e83ee35ba09571f57580",
+    "url": "/vickase.github.io/workout/index.html"
+  },
+  {
+    "revision": "131edc3941fe91f219b7",
+    "url": "/vickase.github.io/workout/static/css/main.34a08be4.chunk.css"
+  },
+  {
+    "revision": "94e6d0d18f5421029520",
+    "url": "/vickase.github.io/workout/static/js/2.e1f9dedd.chunk.js"
+  },
+  {
+    "revision": "a5288a4ed4e4beba8413aa5c9ae969ea",
+    "url": "/vickase.github.io/workout/static/js/2.e1f9dedd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "131edc3941fe91f219b7",
+    "url": "/vickase.github.io/workout/static/js/main.bbd62f8a.chunk.js"
+  },
+  {
+    "revision": "d104ca798c84e04fc88a",
+    "url": "/vickase.github.io/workout/static/js/runtime-main.594d143b.js"
+  }
+]);
