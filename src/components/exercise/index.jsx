3ed016cf19e0c7756c89be9df6950055e416b8ee
Victoria Aseff <vickase.a@gmail.com>
2020-03-06 13:00:09 -0300

    coso

--- src/components/exercise/index.jsx
+++ src/components/exercise/index.jsx
@@ -1,17 +1,34 @@
 import React from "react";
 import "./exercise.css";
 
+import FitnessCenter from "@material-ui/icons/FitnessCenter";
+import Edit from "@material-ui/icons/Edit";
+import Delete from "@material-ui/icons/Delete";
+
 const Exercise = ({ ejercicios }) => {
   return (
     <>
       {ejercicios.map(ejercicio => (
         <div className="exercise-container">
-          <ul className="exercise-list">
-            <h3 className="exercise-name">{ejercicio.exercise}</h3>
-            <li className="exercise-itemlist">
-              {ejercicio.repeticiones}x{ejercicio.sets}
-            </li>
-          </ul>
+          <div className="fitness">
+            <FitnessCenter
+              style={{ fontSize: "80px", color: "#BDFF44", margin: "20px" }}
+            ></FitnessCenter>
+            <ul className="exercise-list">
+              <h3 className="exercise-name">{ejercicio.exercise}</h3>
+              <li className="exercise-itemlist">
+                {ejercicio.repeticiones}x{ejercicio.sets}
+              </li>
+            </ul>
+          </div>
+          <div className="icono">
+            <Edit
+              style={{ fontSize: "40px", color: "#BDFF44", margin: "20px" }}
+            ></Edit>
+            <Delete
+              style={{ fontSize: "40px", color: "#BDFF44", margin: "20px" }}
+            ></Delete>
+          </div>
         </div>
       ))}
     </>
